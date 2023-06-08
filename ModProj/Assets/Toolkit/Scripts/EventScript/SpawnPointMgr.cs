9791a2a8256c7efea4355f1dcbef0d429cac30da
fonzie <fonzieyang@gmail.com>
2023-06-08 10:26:01 +0800

    update SpawnPointMgr

--- ModProj/Assets/Toolkit/Scripts/EventScript/SpawnPointMgr.cs
+++ ModProj/Assets/Toolkit/Scripts/EventScript/SpawnPointMgr.cs
@@ -11,5 +11,18 @@ namespace CrossLink
     public class SpawnPointMgr : MonoBehaviour
     {
         static public SpawnPointMgr Instance;
+        public Transform[] pointList;
+
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < pointList.Length; ++i)
+            {
+                Gizmos.DrawSphere(pointList[i].position, 0.1f);
+                Gizmos.DrawLine(pointList[i].position, pointList[i].position + pointList[i].forward);
+            }
+        }
+#endif
     }
 }
